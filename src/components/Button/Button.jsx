df2dc2e6d343ad2a79a46b62c97e8708323d2178
JISUlicious <ljshong@hanmail.net>
2024-01-12 22:04:52 +0900

    [WEA-8] 💄 Button 완료

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,26 @@
+import './Button.scss';
+/**
+ * Button props list
+ * @property {function} onClick    - 버튼 클릭 시 실행 할 함수
+ * @property {boolean} isDisabled  - 버튼 비활성화 상태
+ * @property {string} text         - 버튼에 표시 할 텍스트
+ */
+
+const Button = props => {
+  const {
+    onClick,
+    text,
+    isDisabled,
+  } = props;
+
+  return (
+    <button 
+      className={`button ${isDisabled ? 'disabled' : ''}`} 
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
