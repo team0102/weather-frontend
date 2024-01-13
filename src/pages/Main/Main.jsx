ddd00ff70780506f063de0d3774ad9d1ea429026
leechengwon <141094801+leechengwon@users.noreply.github.com>
2024-01-13 19:13:27 +0900

    네브컴포넌트 1차 PR

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,26 +1,25 @@
 import { useState } from 'react';
 import Nav from '../../components/Nav/Nav';
-import './Main.scss';
 import Portal from '../../components/Portal/Portal';
+import './Main.scss';
 
 // 1.최상위 메인태그는 항상 파일명을 따라가 네스팅 해줍니다. scss 파일에서
 // 이파일은 Main.jsx 이기때문에 최상위 부모태그 네이밍을 main으로 해주는걸 컨벤션으로합니다.
 
 const Main = () => {
-  const [toggle, setToggle] = useState(false);
-  console.log(toggle);
+  const [navToggle, setNavToggle] = useState(false);
 
   const toggleClick = () => {
-    setToggle(!toggle);
+    setNavToggle(!navToggle);
   };
   return (
     <main className="main">
-      <button onClick={toggleClick}> 네브바 나와</button>
+      <button onClick={toggleClick}>네브바 나와</button>
       {/* <h1>Weather-Project 시작</h1> */}
 
-      {toggle && (
+      {navToggle && (
         <Portal>
-          <Nav setToggle={setToggle} />
+          <Nav setNavToggle={setNavToggle} />
         </Portal>
       )}
     </main>
