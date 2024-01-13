68999107bb7106093c6544549496be0d05386ee3
leechengwon <141094801+leechengwon@users.noreply.github.com>
2024-01-13 19:30:28 +0900

    포탈 아이디 변경

--- src/components/Portal/Portal.jsx
+++ src/components/Portal/Portal.jsx
@@ -2,8 +2,8 @@
 import ReactDom from 'react-dom';
 
 const Portal = ({ children }) => {
-  // dom에서 modal라는 id를 가진 요소를 찾아 el로 정의 (el: element)
-  const el = document.getElementById('modal');
+  // dom에서 portal라는 id를 가진 요소를 찾아 el로 정의 (el: element)
+  const el = document.getElementById('portal');
   // createPortal 메서드를 사용하여 modal 안에 있는 children을 el의 위치에서 렌더링
   return ReactDom.createPortal(children, el);
 };
