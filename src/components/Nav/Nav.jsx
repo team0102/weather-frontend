17c4590fc6d32a829bd8b41c76323c38a25ed9c1
leechengwon <141094801+leechengwon@users.noreply.github.com>
2024-01-13 18:09:13 +0900

    글로벌 네브 개발중//미디어쿼리 설정// 포탈컴포넌트 위치변경//트렌지션개발중

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,44 @@
+import { Link } from 'react-router-dom';
 import './Nav.scss';
 
-const Nav = () => {
-  return <div></div>;
+const Nav = ({ setToggle }) => {
+  const closeNav = () => {
+    setToggle(false);
+  };
+  return (
+    <div className="navWrapContainer">
+      <nav className="nav">
+        <div className="closeBtnWrap">
+          <button onClick={closeNav}>닫기 버튼</button>
+        </div>
+
+        <section className="loginSingUpWrap">
+          <button className="navLoginBtn">Sing In</button>
+          <button className="navJoinBtn">Sing Up</button>
+        </section>
+
+        <section>
+          <ul className="linkWrap">
+            <li>
+              <Link>Weather</Link>
+            </li>
+            <li>
+              <Link>Community</Link>
+            </li>
+            <li>
+              <Link>RealTime Chat</Link>
+            </li>
+            <li>
+              <Link>Hot Fashion</Link>
+            </li>
+            <li>
+              <Link>Preferences</Link>
+            </li>
+          </ul>
+        </section>
+      </nav>
+    </div>
+  );
 };
 
 export default Nav;
