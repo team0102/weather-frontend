10d26df5d570a474dcd608d43c8bf6f4821f034f
JISUlicious <ljshong@hanmail.net>
2024-01-15 16:47:47 +0900

    🚧 리뷰사항 반영 수정

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,22 +1,42 @@
 import './Button.scss';
 /**
  * Button props list
- * @property {function} onClick    - 버튼 클릭 시 실행 할 함수
- * @property {boolean} isDisabled  - 버튼 비활성화 상태
- * @property {string} text         - 버튼에 표시 할 텍스트
+ * @property {string} type: 'submit', 'button', 'reset'         - 버튼의 타입을 지정
+ * @property {string} text                                      - 버튼에 표시 할 텍스트
+ * @property {function} onClick                                 - 버튼 클릭 시 실행 할 함수
+ * @property {string} isDisabled                                - 버튼의 비활성화 상태를 전달
+ * @property {string} backgroundColor                           - 버튼의 바탕 색상을 지정
+ * @property {string} borderColor                               - 버튼의 가장자리 색상을 지정 
+ * @property {string} textColor                                 - 버튼 텍스트의 색상을 지정
+ * @property {string} textWeight                                - 버튼 텍스트의 두께를 지정
  */
 
 const Button = props => {
   const {
-    onClick,
+    type,
     text,
-    isDisabled,
+    onClick,
+    isDisabled = false,
+    backgroundColor,
+    borderColor,
+    textColor,
+    textWeight,
   } = props;
+
+  const buttonStyle = {
+    backgroundColor: backgroundColor,
+    border: `1px solid ${borderColor}`,
+    color: textColor,
+    fontWeight: textWeight, 
+  };
 
   return (
     <button 
-      className={`button ${isDisabled ? 'disabled' : ''}`} 
-      onClick={isDisabled ? null : onClick}
+      type={type}
+      className={`button`} 
+      onClick={onClick}
+      style={isDisabled ? null : buttonStyle}
+      disabled={isDisabled}
     >
       {text}
     </button>
