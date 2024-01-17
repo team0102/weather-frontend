6d679e530183ee92d23140d5e28fd853d5d24a54
Minjae Park <jayy.intheworld@gmail.com>
2024-01-17 10:54:48 +0900

    :lipstick: input 컴포넌트 완료

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,23 +1,14 @@
 import './Input.scss';
 
-const Input = ({
-  type = 'text',
-  placeholder,
-  name,
-  value,
-  onChange,
-  disabled,
-  ...props
-}) => {
+const Input = ({ type = 'text', placeholder, name, value, onChange }) => {
   return (
     <input
+      className="basicInput"
       type={type}
       placeholder={placeholder}
       name={name}
       value={value}
       onChange={onChange}
-      disabled={disabled}
-      {...props}
     />
   );
 };
