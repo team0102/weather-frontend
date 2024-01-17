ed4a231c4a1e863e653a5140d27e3c78ee6cb52e
Minjae Park <jayy.intheworld@gmail.com>
2024-01-17 09:46:09 +0900

    :lipstick: 칩 컴포넌트 완료

--- src/components/Chip/Chip.jsx
+++ src/components/Chip/Chip.jsx
@@ -2,15 +2,10 @@ import './Chip.scss';
 
 /**
  * Chip props list
- * @property {string} id                - 각각의 칩 별로 가지고 있는 고유의 id
- * @property {boolean} checked          - 칩이 선택 된 상태
- * @property {function} defaultChecked  - 기본으로 체크된 칩
- * @property {function} onChange        - 칩의 상태 변경을 위한 함수
- * @property {string} content           - 칩의 내용. 실질적으로 보여지는 부분
- * @property {string} name              - 페이지에서 사용되는 칩 그룹의 이름
+ * @property {string} id      - 라디오 버튼과 해당 라벨을 고유하게 식별하기 위해 사용.
  */
 
-const Chip = ({ id, content, checked, defaultChecked, onChange, ...props }) => {
+const Chip = ({ id, content, checked, onChange, ...props }) => {
   return (
     <div className="chipWrap" {...props}>
       <input
@@ -18,13 +13,10 @@ const Chip = ({ id, content, checked, defaultChecked, onChange, ...props }) => {
         type="radio"
         id={id}
         checked={checked}
-        defaultChecked={defaultChecked}
         onChange={onChange}
         {...props}
       />
-      <label className="chipLabel" htmlFor={id}>
-        {content}
-      </label>
+      <label htmlFor={id}>{content}</label>
     </div>
   );
 };
