7f54ff5d4b39e14dc44cfe98823d541f3f4d73b8
JISUlicious <ljshong@hanmail.net>
2024-01-17 09:05:00 +0900

    IconButton 작업중

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,22 +1,16 @@
 import { useState } from 'react';
-import Nav from '../../components/Nav/Nav';
+import IconButton from '../../components/IconButton/IconButton';
 import './Main.scss';
 
 // 1.최상위 메인태그는 항상 파일명을 따라가 네스팅 해줍니다. scss 파일에서
 // 이파일은 Main.jsx 이기때문에 최상위 부모태그 네이밍을 main으로 해주는걸 컨벤션으로합니다.
+import { ReactComponent as Icon } from '../../svg/Global/Hamburger.svg';
 
 const Main = () => {
-  const [navToggle, setNavToggle] = useState(false);
-
-  const toggleClick = () => {
-    setNavToggle(!navToggle);
-  };
+  console.log(Icon)
   return (
     <main className="main">
-      <button onClick={toggleClick}>네브바 나와</button>
-      {/* <h1>Weather-Project 시작</h1> */}
-
-      <Nav navToggle={navToggle} setNavToggle={setNavToggle} />
+      <IconButton Icon={Icon} text='Menu' isInactive={true} onClick={() => console.log(111)} contentColor={'blue'} iconWidth={'90px'} />
     </main>
   );
 };
