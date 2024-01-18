d4ddeb9e59b2a5b588e6af68c9b6ba2443890c5f
JISUlicious <ljshong@hanmail.net>
2024-01-18 18:04:31 +0900

    Icon asset 추가 출처:https://www.svgrepo.com/collection/dazzle-line-icons

--- src/components/IconButton/IconButton.jsx
+++ src/components/IconButton/IconButton.jsx
@@ -2,28 +2,29 @@ import './IconButton.scss';
 /**
  * IconButton props list
  * @property {string} type: 'submit', 'button', 'reset'                                    - 아이콘 버튼의 타입을 지정
- * @property {ReactComponent} Icon                                                         - 아이콘 버튼에 표시 할 아이콘 컴포넌트
  * @property {function} onClick                                                            - 아이콘 버튼 클릭 시 실행할 동작을 정의한 함수
- * @property {string} size: 'sm', 'md', 'lg'                                               - 아이콘 버튼 텍스트의 두께를 지정
- * @property {string} color: 'primary', 'secondary', 'tertiary', 'light', 'gray', 'dark'   - 아이콘 버튼의 아이콘과 텍스트의 색상을 지정
+ * @property {string} size: 'sm', 'md', 'lg'                                               - 아이콘 버튼의 크기를 지정
+ * @property {string} color: 'primary', 'secondary', 'tertiary', 'light', 'gray', 'dark'   - 아이콘 버튼의 아이콘의 색상을 지정
  */
 
 const IconButton = ({
   type = 'button',
-  Icon,
   onClick,
   size,
   color,
+  children,
 }) => {
 
   return (
-    <button
-      type={type}
-      className={`iconButton ${size ? size : ""} ${color ? color : ""}`}
-      onClick={onClick}
-    >
-      {<Icon className={`icon`}/>}
-    </button>
+    <div className={`iconButtonWrapper ${size ? size : ""}`}>
+      <button
+        type={type}
+        className={`iconButton ${color ? color : ""}`}
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    </div>
   );
 };
 
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,13 +4,15 @@ import './Main.scss';
 
 // 1.최상위 메인태그는 항상 파일명을 따라가 네스팅 해줍니다. scss 파일에서
 // 이파일은 Main.jsx 이기때문에 최상위 부모태그 네이밍을 main으로 해주는걸 컨벤션으로합니다.
-import { ReactComponent as Icon } from '../../svg/Global/Hamburger.svg';
+import { ReactComponent as Icon } from '../../svg/Global/Image.svg';
 
 const Main = () => {
   console.log(Icon)
   return (
     <main className="main">
-      <IconButton Icon={Icon} onClick={() => console.log(111)} color='primary' />
+      <IconButton onClick={() => console.log(111)} color='secondary' size='xlg'>
+        <Icon />
+      </IconButton>
     </main>
   );
 };
