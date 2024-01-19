26af281a26d102e21c5a08d44df71f54d15b953b
Minjae Park <jayy.intheworld@gmail.com>
2024-01-19 19:14:03 +0900

    :art: 주석 추가

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,16 +1,37 @@
 import './Input.scss';
 
-const Input = ({ type = 'text', placeholder, name, value, onChange }) => {
+/**
+ *@property {string} type                    - 인풋의 타입. text를 기본으로 지정함
+ *@property {string} label                   - 인풋의 라벨
+ *@property {string} placeholder             - 인풋에 입력 될 내용에 대한 미리보기
+ *@property {string} name                    - 개별 인풋을 구별하는 값
+ *@property {string} value                   - 인풋에 입력되는 입력값
+ *@property {function} onChange              - 입력시 실행되는 함수
+ */
+
+const Input = ({
+  type = 'text',
+  id,
+  label,
+  placeholder,
+  name,
+  value,
+  onChange,
+}) => {
   return (
-    <input
-      className="basicInput"
-      type={type}
-      placeholder={placeholder}
-      name={name}
-      value={value}
-      onChange={onChange}
-      onWheel={e => e.target.blur()} // 마우스 휠 스크롤 발생시 input focus 제거
-    />
+    <div>
+      <label htmlFor={id}>{label}</label>
+      <input
+        className="basicInput"
+        type={type}
+        id={id}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+        onWheel={e => e.target.blur()} // 마우스 휠 스크롤 발생시 input focus 제거
+      />
+    </div>
   );
 };
 
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,7 +5,6 @@ import Nav from '../../components/Nav/Nav';
 
 import './Main.scss';
 import TestModal from '../../components/Modal/TestModal';
-import Input from '../../components/Input/Input';
 
 // 1.최상위 메인태그는 항상 파일명을 따라가 네스팅 해줍니다. scss 파일에서
 // 이파일은 Main.jsx 이기때문에 최상위 부모태그 네이밍을 main으로 해주는걸 컨벤션으로합니다.
@@ -46,8 +45,6 @@ const Main = () => {
           isModalOpen={isModalOpen} // useState 값
         />
       )}
-
-      <Input type="number" placeholder="입력해주세요" />
 
       <Nav
         navToggle={navToggle}
