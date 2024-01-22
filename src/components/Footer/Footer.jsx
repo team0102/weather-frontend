b860814313a344ffbbeb6ae4b21e9d7123657d05
Minjae Park <jayy.intheworld@gmail.com>
2024-01-22 21:33:28 +0900

    :lipstick: web footer 완료

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { FOOTER_ICON_DATA } from '../../data/FooterIconData';
+import { FOOTER_ICON_DATA } from '../../data/FooterData/FooterIconData';
 import IconButton from '../IconButton/IconButton';
 import './Footer.scss';
 
@@ -6,7 +6,14 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="webFooter">
-        <div>web</div>
+        <ul className="footerList">
+          <li>회사소개</li>
+          <li>이용약관</li>
+          <li>공지사항</li>
+          <li>찾아오시는길</li>
+          <li>개인정보처리방침</li>
+        </ul>
+        <span className="footerCopyright">© 2024 WEATHER Project</span>
       </div>
       <div className="deviceFooter">
         {FOOTER_ICON_DATA.map(({ color, size, content, label }) => (
---
+++ src/data/FooterData/FooterItemData.jsx
@@ -0,0 +1,22 @@
+export const FOOTER_ITEM_DATA = [
+  {
+    id: '1',
+    content: '회사소개',
+  },
+  {
+    id: '2',
+    content: '공지사항',
+  },
+  {
+    id: '3',
+    content: '이용약관',
+  },
+  {
+    id: '4',
+    content: '회사소개',
+  },
+  {
+    id: '5',
+    content: '개인정보처리방침',
+  },
+];
