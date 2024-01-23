0aa241677c506c86c3a27294e1c08e0cb3d24cd4
Minjae Park <jayy.intheworld@gmail.com>
2024-01-24 00:30:19 +0900

    :art: white-space 추가

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -7,10 +7,10 @@ import './Main.scss';
 import SelectBox from '../../components/SelectBox/SelectBox';
 import TestModal from '../../components/Modal/TestModal';
 
-
 // 1.최상위 메인태그는 항상 파일명을 따라가 네스팅 해줍니다. scss 파일에서
 // 이파일은 Main.jsx 이기때문에 최상위 부모태그 네이밍을 main으로 해주는걸 컨벤션으로합니다.
 import { ReactComponent as Icon } from '../../svg/Global/Image.svg';
+import ChipGroup from '../../components/Chip/ChipGroup';
 
 const Main = () => {
   //부모라고 가정한 모달에 필요한 함수,useState 입니다.
@@ -30,13 +30,29 @@ const Main = () => {
   const [selected, setSelected] = useState(2);
   return (
     <main className="main">
-
       {selected}
-      <SelectBox options={{1: "aaaa", 2: "bbbb "}} onChange={setSelected} selected={selected} />
+      <SelectBox
+        options={{ 1: 'aaaa', 2: 'bbbb ' }}
+        onChange={setSelected}
+        selected={selected}
+      />
 
-      <IconButton onClick={() => console.log(111)} color='secondary' size='xlg' content="Home" />
+      <IconButton
+        onClick={() => console.log(111)}
+        color="secondary"
+        size="xlg"
+        content="Home"
+      />
 
-      <Button size='md' color='primary' style='outline' isDisabled={false} onClick={() => console.log("clicked")}>Children</Button>
+      <Button
+        size="md"
+        color="primary"
+        style="outline"
+        isDisabled={false}
+        onClick={() => console.log('clicked')}
+      >
+        Children
+      </Button>
       <button onClick={toggleClick}>네브바 나와</button>
       <button onClick={handleModalToggle}>모달 나와</button>
 
@@ -47,6 +63,8 @@ const Main = () => {
           isModalOpen={isModalOpen} // useState 값
         />
       )}
+
+      <ChipGroup />
 
       <Nav
         navToggle={navToggle}
