78adc51d55dec52f8a7ddae8842c578f69ba1747
JISUlicious <ljshong@hanmail.net>
2024-03-08 22:51:10 +0900

    chat input ui

--- src/pages/Chat/components/ChatInputSection/ChatInputSection.jsx
+++ src/pages/Chat/components/ChatInputSection/ChatInputSection.jsx
@@ -1,9 +1,29 @@
+import { useState } from 'react';
 
+import Input from '../../../../components/Input/Input';
+import IconButton from '../../../../components/IconButton/IconButton';
+
+import './ChatInputSection.scss';
 
 const ChatInputSection = () => {
-  return <div className="chatInputSection section">
-    input
-  </div>
+  const [inputValue, setInputValue] = useState('');
+
+  const onClickChatButton = () => {
+    console.log('send message', inputValue);
+    setInputValue('');
+  };
+
+  return (
+    <div className="chatInputSection section">
+      <Input
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
+      ></Input>
+      <div className="chatButtonWrap">
+        <IconButton content="ChatRound" size="lg" color="light" onClick={onClickChatButton}></IconButton>
+      </div>
+    </div>
+  );
 };
 
 export default ChatInputSection;
